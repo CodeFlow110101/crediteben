32dcbf243796a99ed711ecae5cc4fc5150495a6c
Nishant Kedare <kedarenishant11111@gmail.com>
2025-11-20 20:03:25 +0530

    Career jobs design change

--- resources/views/livewire/career.blade.php
+++ resources/views/livewire/career.blade.php
@@ -25,11 +25,18 @@ mount(fn($isfrench) => $this->isfrench = $isfrench);
 
         <div class="py-10 flex flex-col gap-6">
             @foreach($jobs as $job)
-            <div class="flex flex-col gap-4 border border-primary p-4">
+            <div x-data="{ expanded: false }" class="flex flex-col gap-4 border-t border-t-primary p-4">
                 <div class="text-primary text-xl font-semibold uppercase">
                     {{ $isfrench ? $job->title_fr : $job->title_en }}
                 </div>
-                <div class="font-light max-xl:text-sm">
+                <div x-show="!expanded" class="font-light max-xl:text-sm">
+                    {!! Str::of($isfrench ? strip_tags($job->description_fr) :strip_tags($job->description_en))->limit("200") !!}
+                    @if(Str::of($isfrench ? strip_tags($job->description_fr) :strip_tags($job->description_en))->length > 200)
+                    <div @click="expanded = !expanded;" class="text-sm text-primary cursor-pointer underline">Read more</div>
+                    @endif
+                </div>
+
+                <div x-show="expanded" class="font-light max-xl:text-sm">
                     {!! Str::of($isfrench ? $job->description_fr : $job->description_en) !!}
                 </div>
             </div>
